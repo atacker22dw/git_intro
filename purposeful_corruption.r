d981211edc9f7f3026d2eb688db8079a18de35c1
atacker22dw <72352489+atacker22dw@users.noreply.github.com>
2024-01-28 10:34:55 -0500

    Create purposeful_corruption.r
    class exercise.  students should fork the repository, find and correct the mistakes, and push the update.  I will select one fix to then merge with the main

---
+++ purposeful_corruption.r
@@ -0,0 +1,8 @@
+#This is a purposefully corrupted file.  Fork this repository, find and correct my mistake(s) locally.  Then submit a pull request announcing said change.
+
+x >- 5
+y >- 1
+
+while(y < x){
+  print("y is still less than x")
+}
