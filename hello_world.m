ea20b1ab0340dd8d9018868bb1746dbc8b3f55c2
atacker22dw <72352489+atacker22dw@users.noreply.github.com>
2024-01-28 09:42:43 -0500

    Add files via upload
    hello world in matlab

---
+++ hello_world.m
@@ -0,0 +1,12 @@
+%hello world matlab
+%git can host a repo with multiple different languages simultaneously
+%however unless explicitly coerced, do not expect the files from different
+%languages to interact effectively
+
+x = "hello world";
+
+for i = 1:5
+    x
+end
+
+   
