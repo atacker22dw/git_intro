ab5e4e456372cdcafa8757a331e2b73d99298556
Yuewei Wang <118140937+YueweiWang2@users.noreply.github.com>
2024-02-02 08:32:24 -0500

    Update purposeful_corruption.r

--- purposeful_corruption.r
+++ purposeful_corruption.r
@@ -1,6 +1,6 @@
 #This is a purposefully corrupted file.  Fork this repository, find and correct my mistake(s) locally.  Then submit a pull request announcing said change.
 
-x >- 5
+x > 5
 y >- 1
 
 while(y < x){
