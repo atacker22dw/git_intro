2bc8319ec02f5e972310dd4a8d9ec2761db20d68
atacker22dw <72352489+atacker22dw@users.noreply.github.com>
2024-09-11 17:04:23 -0400

    Update Hello_World_deviation.r

--- Hello_World_deviation.r
+++ Hello_World_deviation.r
@@ -1,1 +1,14 @@
 #update this with the Hello_World_deviation.r file from the patch branch after ensuring that it runs locally
+
+print("Please enter a number of times you would like to repeat Hello World")
+#read in an integer
+var = readline();
+# convert the inputted value to integer
+var = as.integer(var);
+#while loop
+i=1
+while(i < (var+1)){
+  print("Hello World")
+  i=i+1
+}
+  
