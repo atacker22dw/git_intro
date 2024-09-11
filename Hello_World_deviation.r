7d70839d57c6cdd9426e76302131f4ee489c46a8
atacker22dw <72352489+atacker22dw@users.noreply.github.com>
2024-09-11 17:36:46 -0400

    Update Hello_World_deviation.r

--- Hello_World_deviation.r
+++ Hello_World_deviation.r
@@ -2,16 +2,3 @@
 #this should prompt the user to enter an integer number of times to print Hello World. 
 #it should store this user input as an an integer, var, and then use a while loop to print Hello World that number of times.  
 
-print("Please enter a number of times you would like to repeat Hello World")
-#read in an integer
-var = readline();
-# convert the inputted value to integer
-var = as.integer(var);
-#while loop
-i=1
-while(i < (var+1)){
-  print("Hello World")
-  i=i+1
-}
-
-#we have updated this file.  
