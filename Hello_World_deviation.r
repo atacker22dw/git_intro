207aca13ac15c18980ed3d1be155c920b49f87b5
atacker22dw <72352489+atacker22dw@users.noreply.github.com>
2024-09-11 17:24:35 -0400

    Update Hello_World_deviation.r

--- Hello_World_deviation.r
+++ Hello_World_deviation.r
@@ -3,4 +3,14 @@
 #this should prompt a user to enter a number of times to print hello world, store that user response as an 
 #integer and then use a while loop to print hello world that number of times to the screen
 
-
+print("Please enter a number of times you would like to repeat Hello World")
+#read in an integer
+var = readline();
+# convert the inputted value to integer
+var = as.integer(var);
+#while loop
+i=1
+while(i < (var+1)){
+  print("Hello World")
+  i=i+1
+}
