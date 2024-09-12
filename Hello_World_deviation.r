0fc3ad11b13b153f66db194f9996dc007029b3b6
atacker22dw <72352489+atacker22dw@users.noreply.github.com>
2024-09-12 08:27:04 -0400

    Update Hello_World_deviation.r

--- Hello_World_deviation.r
+++ Hello_World_deviation.r
@@ -1,3 +1,17 @@
 #update this with the Hello_World_deviation.r file from the patch branch after ensuring that it runs locally
 #this should prompt the user to enter an integer number of times to print Hello World. 
 #it should store this user input as an an integer, var, and then use a while loop to print Hello World that number of times.  
+
+print("Please enter a positive integer number of times you would like to repeat Hello World")
+#read in integer
+var = readline();
+#coerce value to integer if not already
+var = as.integer(var);
+#loop to iterate and print
+i = 1
+while(i<(var+1)){
+  print("Hello World")
+  i = i+1
+}
+
+
